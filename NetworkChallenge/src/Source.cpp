db8c693b429751b17301baf8276920c6bd3ce763
Jake Swartwout <jacob.swartwout@gmail.com>
2020-03-01 18:41:18 -0700

    Plz

---
+++ NetworkChallenge/src/Source.cpp
@@ -0,0 +1,82 @@
+
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_PORT "27015"
+
+int main(int argc, char* argv[])
+{
+
+    WSADATA wsaData;
+
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    std::cout << "Load Success" << std::endl;
+
+    struct addrinfo* result = NULL,
+        * ptr = NULL,
+        hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+
+    
+
+    // Resolve the server address and port
+    iResult = getaddrinfo("10.202.130.237", DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+
+	// Create a SOCKET for the server to listen for client connections
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+
+	/*if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		printf("Listen failed with error: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}*/
+}
+
