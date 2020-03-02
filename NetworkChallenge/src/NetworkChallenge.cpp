5925ae19c1fe255bb698ef51f1e2df5dbbf988fa
Spas Angelov <spasangelov@gmail.com>
2020-03-01 18:04:56 -0700

    Other Stuff

--- NetworkChallenge/src/NetworkChallenge.cpp
+++ NetworkChallenge/src/NetworkChallenge.cpp
@@ -6,7 +6,9 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int main()
+#define DEFAULT_PORT "27015"
+
+int main(int argc, char* argv[])
 {
 
     WSADATA wsaData;
@@ -22,7 +24,45 @@ int main()
 
     std::cout << "Load Success" << std::endl;
 
+    struct addrinfo* result = NULL,
+        * ptr = NULL,
+        hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
 
 
+    
+
+    // Resolve the server address and port
+    iResult = getaddrinfo("1", DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+
+    // Attempt to connect to the first address returned by
+    // the call to getaddrinfo
+    ptr = result;
+
+    // Create a SOCKET for connecting to server
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+    return 1;
+}
 }
 
