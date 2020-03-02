ddd58e91f5a71cca529738cb0d0fb04290074fe4
Spas Angelov <spasangelov@gmail.com>
2020-03-01 17:55:28 -0700

    My files and prefs

--- NetworkChallenge/src/NetworkChallenge.cpp
+++ NetworkChallenge/src/NetworkChallenge.cpp
@@ -1,14 +1,26 @@
 
-
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 int main()
 {
 
+    WSADATA wsaData;
 
+    int iResult;
 
-    std::cout << "Hello World!\n";
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
 
+    std::cout << "Load Success" << std::endl;
 
 
 
